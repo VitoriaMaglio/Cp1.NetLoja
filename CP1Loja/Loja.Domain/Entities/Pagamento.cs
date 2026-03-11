bcbc7beb71c6aa317bfafc94e8edde5ed6c94b18
VitoriaMaglio <vitoriamaglio@gmail.com>
2026-03-11 16:55:59 -0300

    feat: implementa entidade Pagamento.

---
+++ CP1Loja/Loja.Domain/Entities/Pagamento.cs
@@ -0,0 +1,19 @@
+using Loja.Domain.Commons;
+
+namespace Loja.Domain.Entities;
+
+public class Pagamento : BaseEntity
+{
+    public String TipoPagamento { get; private set; }
+    public decimal Valor { get; private set; }
+    public DateTime Data { get; private set; }
+    public Pedido Pedido { get; private set; }
+    //1 Pedido tem 1 Pagamento 
+
+    public Pagamento(String tipoPagamento, decimal valor, DateTime data)
+    {
+        TipoPagamento = tipoPagamento;
+        Valor = valor;
+        Data = data;
+    }
+}
