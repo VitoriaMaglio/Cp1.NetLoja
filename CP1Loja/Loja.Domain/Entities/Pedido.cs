f66041f6b9ec3b9fe851272f635f6a3b32d86422
VitoriaMaglio <vitoriamaglio@gmail.com>
2026-03-11 11:53:54 -0300

    feat: implementa entidade Pedido

---
+++ CP1Loja/Loja.Domain/Entities/Pedido.cs
@@ -0,0 +1,28 @@
+using System.Runtime.InteropServices.JavaScript;
+using Loja.Domain.Commons;
+
+namespace Loja.Domain.Entities;
+
+public class Pedido : BaseEntity
+{
+    public DateTime DataPedido { get; private set; }
+
+    public string Status { get; private set; }
+
+    public decimal ValorTotal { get; private set; }
+    
+    //FK relacionamento com Cliente N-1 muitos pedidos pertencem a um cliente
+    public int IdCliente { get; private set; }
+    
+    //Relacionamento N-1 
+    //Lado N tem o objeto de navegação
+    //Lado 1 tem a lista
+    public Cliente Cliente { get; set; }
+
+    //Um pedido possui vários itens 1-N
+    public List<ItemPedido> Itens { get; set; } = new();
+
+    //1-1 Um pedido tem 1 pagamento
+    public Pagamento Pagamento { get; set; }
+
+}
