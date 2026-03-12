83076c3e31278cf784d8babb1414d7910d564a2d
FelipeMaglio <felipemagliofilho@gmail.com>
2026-03-12 10:48:39 -0300

    fix: Adicionando opcionalidade.

--- CP1Loja/Loja.Application/obj/Debug/net10.0/Loja.Application.AssemblyInfo.cs
+++ CP1Loja/Loja.Application/obj/Debug/net10.0/Loja.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a32595abb9c828e95adb4e8ac3bdf5eb5411739")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e06f5ca70679fa135870a20dda4cfc869337bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
